d678f607bf0a1ff80b82106d2c70a4612a9d2458
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-10-31 00:12:58 +0700

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from keras.models import model_from_json
 from keras.preprocessing import image
 import streamlit as st
 import time
+import os
 
 st.write("Current directory: ", os.getcwd())  # Check current working directory
 st.write("Model path exists: ", os.path.exists("Model/Facial_Expression_Recognition.json"))  # Check if model path is valid
