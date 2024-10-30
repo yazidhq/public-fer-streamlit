cca28fa36f2bff7f5457df39ab88c648d80cc054
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-10-31 00:14:48 +0700

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
-import cv2
-import numpy as np
-from keras.models import model_from_json
-from keras.preprocessing import image
+
 import streamlit as st
-import time
 import os
 
 st.write("Current directory: ", os.getcwd())  # Check current working directory
