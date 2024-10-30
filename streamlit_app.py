3114fa82061309b8b95587e0265628797fa3e28b
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-10-31 00:37:18 +0700

    debugging camera

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,9 @@
 import cv2
 import streamlit as st
 
-# Streamlit app
 st.title("Webcam Test")
 
-# Placeholder for video feed
-video_placeholder = st.empty()
-
-# Open a connection to the camera
+# Start video capture
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -15,7 +11,7 @@ if not cap.isOpened():
 else:
     ret, frame = cap.read()
     if ret:
-        video_placeholder.image(frame, channels="BGR", caption="Webcam Feed", use_column_width=True)
+        st.image(frame, channels="BGR", caption="Webcam Feed", use_column_width=True)
     else:
         st.error("Failed to capture image.")
 
