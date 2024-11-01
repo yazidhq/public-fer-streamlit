3736553f681012fd58e640ab1edb4b02e48d2833
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-11-01 22:58:14 +0700

    update: test camera

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,8 @@ video_placeholder = st.empty()
 # Open a connection to the camera
 cap = cv2.VideoCapture(0) 
 if not cap.isOpened():
-    st.error("Could not access the webcam. Please check your camera settings.")
+    cap.release()
+    # st.error("Could not access the webcam. Please check your camera settings.")
 else:
     if 'run_webcam' not in st.session_state:
         st.session_state.run_webcam = False
