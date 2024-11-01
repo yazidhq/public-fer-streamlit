59c5c2454c17c9e9469b4a4b91c8dbb2d2e1ae1c
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-11-01 23:26:04 +0700

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-st.title("Real-Time Facial Emotion Recognition")
+st.title("Facial Emotion Recognition")
 
 # Embed the Flask video stream in Streamlit
-st.markdown(
-    """
-    <iframe src="http://localhost:5000/video_feed" width="700" height="500" frameborder="0" allowfullscreen></iframe>
-    """,
-    unsafe_allow_html=True
-)
+st.markdown("""<iframe src="http://localhost:5000/video_feed" width="700" height="500" frameborder="0" allowfullscreen></iframe>""",unsafe_allow_html=True)
