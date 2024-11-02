c914ea0713d12edd7abaa084ae161ca6bb37b151
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-11-02 09:52:34 +0700

    update: wh mark

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Facial Emotion Recognition")
 
 # Embed the Flask video stream in Streamlit
-st.markdown("""<iframe src="http://localhost:5000" width="700" height="500" frameborder="0" allowfullscreen></iframe>""",unsafe_allow_html=True)
+st.markdown("""<iframe src="http://localhost:5000" frameborder="0" allowfullscreen></iframe>""",unsafe_allow_html=True)
