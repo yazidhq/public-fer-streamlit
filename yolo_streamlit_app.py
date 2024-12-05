574d495a7a170bb41bfe1c433ce4dd3bc0ba90fb
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-12-05 10:55:53 +0700

    feat: file upload

--- yolo_streamlit_app.py
+++ yolo_streamlit_app.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-model_path = 'Yolo_Model/best.pt'
+model_path = 'Yolo_Model/stranger-model.pt'
 model = YOLO(model_path)
 
 def detect_expression(frame):
@@ -11,10 +11,16 @@ def detect_expression(frame):
     annotated_frame = results[0].plot()
     return annotated_frame
 
-st.title("Real-Time Facial Expression Detection")
-st.markdown("Upload a video feed from your webcam and detect facial expressions in real-time!")
+st.title("Facial Emotion Detection")
+st.markdown("Upload a photo or use your webcam to detect facial expressions!")
 
-camera_active = st.sidebar.checkbox("Activate Camera", value=True)
+camera_active = st.sidebar.checkbox("Activate Camera", value=False)
+
+if camera_active:
+    st.sidebar.info("Photo upload is disabled while the camera is active.")
+    upload_photo = None
+else:
+    upload_photo = st.sidebar.file_uploader("Upload a photo", type=["jpg", "jpeg", "png"])
 
 if camera_active:
     cap = cv2.VideoCapture(0)
@@ -34,3 +40,13 @@ if camera_active:
             stframe.image(annotated_frame, channels="RGB")
 
         cap.release()
+
+if upload_photo:
+    file_bytes = np.asarray(bytearray(upload_photo.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    st.image(image_rgb, caption="Uploaded Image", use_column_width=True, channels="RGB")
+    
+    annotated_image = detect_expression(image_rgb)
+    st.image(annotated_image, caption="Detected Expressions", use_column_width=True, channels="RGB")
